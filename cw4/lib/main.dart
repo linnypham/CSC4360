8791be65ecbb751015b8c983011df1058857392b
linnypham <89488749+linnypham@users.noreply.github.com>
2025-03-09 00:16:17 -0500

    cw4

--- cw4/lib/main.dart
+++ cw4/lib/main.dart
@@ -1,122 +1,152 @@
 import 'package:flutter/material.dart';
-
+import 'package:table_calendar/table_calendar.dart';
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PlanManagerScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class Plan {
+  String name;
+  String description;
+  DateTime date;
+  bool isCompleted;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Plan({required this.name, required this.description, required this.date, this.isCompleted = false});
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class PlanManagerScreen extends StatefulWidget {
+  @override
+  _PlanManagerScreenState createState() => _PlanManagerScreenState();
+}
 
-  void _incrementCounter() {
+class _PlanManagerScreenState extends State<PlanManagerScreen> {
+  List<Plan> plans = [];
+
+  void _addPlan(String name, String description, DateTime date) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      plans.add(Plan(name: name, description: description, date: date));
     });
+  }
+
+  void _updatePlan(int index, String newName, String newDescription, DateTime newDate) {
+    setState(() {
+      plans[index].name = newName;
+      plans[index].description = newDescription;
+      plans[index].date = newDate;
+    });
+  }
+
+  void _toggleCompletion(int index) {
+    setState(() {
+      plans[index].isCompleted = !plans[index].isCompleted;
+    });
+  }
+
+  void _deletePlan(int index) {
+    setState(() {
+      plans.removeAt(index);
+    });
+  }
+  
+  void _showPlanDialog({int? index}) {
+    String name = index != null ? plans[index].name : '';
+    String description = index != null ? plans[index].description : '';
+    DateTime date = index != null ? plans[index].date : DateTime.now();
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(index == null ? 'Create Plan' : 'Edit Plan'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Plan Name'),
+                onChanged: (value) => name = value,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Description'),
+                onChanged: (value) => description = value,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: date,
+                    firstDate: DateTime.now(),
+                    lastDate: DateTime(2100),
+                  );
+                  if (pickedDate != null) {
+                    date = pickedDate;
+                  }
+                },
+                child: Text('Select Date'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text('Cancel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (index == null) {
+                  _addPlan(name, description, date);
+                } else {
+                  _updatePlan(index, name, description, date);
+                }
+                Navigator.pop(context);
+              },
+              child: Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      appBar: AppBar(title: Text('Calendar')),
+      body: ListView.builder(
+        itemCount: plans.length,
+        itemBuilder: (context, index) {
+          final plan = plans[index];
+          return Dismissible(
+            key: Key(plan.name),
+            onDismissed: (direction) => _toggleCompletion(index),
+            background: Container(color: Colors.green),
+            child: GestureDetector(
+              onLongPress: () => _showPlanDialog(index: index),
+              onDoubleTap: () => _deletePlan(index),
+              child: ListTile(
+                title: Text(plan.name,
+                    style: TextStyle(
+                        decoration: plan.isCompleted
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none)),
+                subtitle: Text(plan.description),
+                trailing: Icon(plan.isCompleted ? Icons.check : Icons.pending),
+              ),
             ),
-          ],
-        ),
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () => _showPlanDialog(),
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
