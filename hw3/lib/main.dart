5d3a6f521d2f431a4f5c3bcd19eda4ad52e97697
linnypham <89488749+linnypham@users.noreply.github.com>
2025-03-14 15:11:02 -0400

    pairs

--- hw3/lib/main.dart
+++ hw3/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:playing_cards/playing_cards.dart';
 
@@ -10,22 +11,67 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const title = 'Cards';
+    const title = 'Card Pairs';
 
     return MaterialApp(
       title: title,
       home: Scaffold(
         appBar: AppBar(title: const Text(title)),
-        body: GridView.count(
-          crossAxisCount: 4, //columns
-          children: List.generate(16, (index) { //cards amount
-            return Center(
-              child: PlayingCardView(
-                card: PlayingCard(Suit.clubs, CardValue.values[index % CardValue.values.length])
-              ),
-            );
-          }),
-        ),
+        body: CardGrid(),
+      ),
+    );
+  }
+}
+
+class CardGrid extends StatefulWidget {
+  @override
+  _CardGridState createState() => _CardGridState();
+}
+
+class _CardGridState extends State<CardGrid> {
+  late List<PlayingCard> cards;
+
+  @override
+  void initState() {
+    super.initState();
+    cards = _generateCardPairs();
+  }
+
+  List<PlayingCard> _generateCardPairs() {
+    final random = Random();
+    List<PlayingCard> uniqueCards = [];
+
+    while (uniqueCards.length < 8) {
+      var suit = Suit.values[random.nextInt(Suit.values.length)];
+      var value = CardValue.values[random.nextInt(CardValue.values.length)];
+      var newCard = PlayingCard(suit, value);
+
+      if (!uniqueCards.contains(newCard)) {
+        uniqueCards.add(newCard);
+      }
+    }
+
+    List<PlayingCard> pairedCards = [...uniqueCards, ...uniqueCards];
+
+    pairedCards.shuffle();
+
+    return pairedCards;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GridView.count(
+        crossAxisCount: 4, 
+        children: List.generate(cards.length, (index) {
+          return Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: PlayingCardView(
+              card: cards[index],
+              showBack: true),
+          );
+        }),
       ),
     );
   }
