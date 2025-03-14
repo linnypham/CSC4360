74ba0327cf3f45828705d77f6f52dd8cda02b5ed
linnypham <89488749+linnypham@users.noreply.github.com>
2025-03-14 15:24:20 -0400

    card images

--- hw3/lib/main.dart
+++ hw3/lib/main.dart
@@ -1,6 +1,7 @@
+import 'dart:async';
 import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:playing_cards/playing_cards.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,68 +12,152 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const title = 'Card Pairs';
-
-    return MaterialApp(
-      title: title,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(title)),
-        body: CardGrid(),
+    return ChangeNotifierProvider(
+      create: (_) => GameProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: CardMatchingGame(),
       ),
     );
   }
 }
 
-class CardGrid extends StatefulWidget {
-  @override
-  _CardGridState createState() => _CardGridState();
+class CardModel {
+  final String id;
+  final String image;
+  bool isFaceUp;
+  bool isMatched;
+
+  CardModel({required this.id, required this.image, this.isFaceUp = false, this.isMatched = false});
 }
 
-class _CardGridState extends State<CardGrid> {
-  late List<PlayingCard> cards;
+class GameProvider extends ChangeNotifier {
+  List<CardModel> cards = [];
+  CardModel? firstSelected;
+  bool isChecking = false;
 
-  @override
-  void initState() {
-    super.initState();
-    cards = _generateCardPairs();
+  GameProvider() {
+    _initializeGame();
   }
 
-  List<PlayingCard> _generateCardPairs() {
-    final random = Random();
-    List<PlayingCard> uniqueCards = [];
+  void _initializeGame() {
+    List<String> images = List.generate(8, (index) => 'assets/card_$index.png');
+    List<String> pairedImages = [...images, ...images];
+    pairedImages.shuffle(Random());
 
-    while (uniqueCards.length < 8) {
-      var suit = Suit.values[random.nextInt(Suit.values.length)];
-      var value = CardValue.values[random.nextInt(CardValue.values.length)];
-      var newCard = PlayingCard(suit, value);
+    cards = pairedImages.map((img) => CardModel(id: UniqueKey().toString(), image: img)).toList();
+    notifyListeners();
+  }
 
-      if (!uniqueCards.contains(newCard)) {
-        uniqueCards.add(newCard);
-      }
+  void flipCard(CardModel card) {
+    if (isChecking || card.isFaceUp || card.isMatched) return;
+
+    card.isFaceUp = true;
+    notifyListeners();
+
+    if (firstSelected == null) {
+      firstSelected = card;
+    } else {
+      _checkMatch(card);
+    }
+  }
+
+  void _checkMatch(CardModel secondSelected) async {
+    isChecking = true;
+    await Future.delayed(const Duration(seconds: 1));
+
+    if (firstSelected!.image == secondSelected.image) {
+      firstSelected!.isMatched = true;
+      secondSelected.isMatched = true;
+    } else {
+      firstSelected!.isFaceUp = false;
+      secondSelected.isFaceUp = false;
     }
 
-    List<PlayingCard> pairedCards = [...uniqueCards, ...uniqueCards];
-
-    pairedCards.shuffle();
-
-    return pairedCards;
+    firstSelected = null;
+    isChecking = false;
+    notifyListeners();
   }
+}
+
+class CardMatchingGame extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Card Matching Game')),
+      body: Consumer<GameProvider>(
+        builder: (context, game, child) {
+          return GridView.builder(
+            padding: const EdgeInsets.all(16),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+              childAspectRatio: 0.7,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+            ),
+            itemCount: game.cards.length,
+            itemBuilder: (context, index) {
+              return CardWidget(card: game.cards[index]);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class CardWidget extends StatelessWidget {
+  final CardModel card;
+  const CardWidget({super.key, required this.card});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: GridView.count(
-        crossAxisCount: 4, 
-        children: List.generate(cards.length, (index) {
-          return Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: PlayingCardView(
-              card: cards[index],
-              showBack: true),
-          );
-        }),
+    return GestureDetector(
+      onTap: () => context.read<GameProvider>().flipCard(card),
+      child: AnimatedSwitcher(
+        duration: const Duration(milliseconds: 500),
+        transitionBuilder: (child, animation) {
+          return RotationYTransition(child: child, animation: animation);
+        },
+        child: card.isFaceUp || card.isMatched
+            ? Image.asset(card.image, key: ValueKey(card.id))
+            : Container(
+                key: ValueKey('back_${card.id}'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
       ),
+    );
+  }
+}
+
+class RotationYTransition extends StatelessWidget {
+  final Widget child;
+  final Animation<double> animation;
+
+  const RotationYTransition({super.key, required this.child, required this.animation});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (context, child) {
+        final angle = animation.value * pi;
+        return Transform(
+          alignment: Alignment.center,
+          transform: Matrix4.rotationY(angle),
+          child: animation.value > 0.5
+              ? Transform(
+                  alignment: Alignment.center,
+                  transform: Matrix4.rotationY(pi),
+                  child: child,
+                )
+              : child,
+        );
+      },
+      child: child,
     );
   }
 }
